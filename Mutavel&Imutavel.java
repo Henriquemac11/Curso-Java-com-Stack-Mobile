1113c30729b922973d09d0d1181a45d22c18a1f7
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-09-08 23:12:26 -0300

    Create Mutavel&Imutavel.java

---
+++ Mutavel&Imutavel.java
@@ -0,0 +1,18 @@
+public class Main2 {
+    public static void main(String[] args) {
+
+        //Mutabilidade e Imutabilidade
+        //Variavel Mutavel
+        byte idade = 30;
+
+        idade = 50;
+
+        System.out.println(idade);
+
+        //Variavel Imutavel
+        final String NOME = "Henrique";  //final = costante
+        //Recomendacao: Toda variavel costante, seja em letra maiscula
+
+
+    }
+}
