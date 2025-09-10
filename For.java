3ae6c4b34a74df4ee450cc58ee3bdf29df63494a
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-09-10 14:31:05 -0300

    Create For.java

---
+++ For.java
@@ -0,0 +1,10 @@
+public class For {
+    public static void main(String[] args) {
+
+        String[] listaCompras = {"Banana", "Carne", "Feijão", "Arroz",};
+
+        for(int i = 0; listaCompras.length < 5; i++){
+        System.out.println(listaCompras[i]);
+        }
+    }
+}
