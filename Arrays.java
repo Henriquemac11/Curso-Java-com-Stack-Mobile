87b623f29e810141ffcf29c73dacf1486f821cdb
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-09-10 13:14:39 -0300

    Create Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class Arrays {
+    public static void main(String[] args){
+
+        //Arrays
+        //São super variaveis que aguetam varios valores;
+
+                               //   0        1         2         3
+        String[] listaCompras = {"Banana", "Carne", "Feijão", "Arroz",};
+        int[] num = {10, 20, 30, 40, 50};
+        double[] notas = {8.5, 10.0, 7.9, 9.6, 3.6};
+        boolean[] estados = {true, false, true};
+
+        System.out.println("Lista de compras:");
+        System.out.println(java.util.Arrays.toString(listaCompras));
+
+
+    }
+}
