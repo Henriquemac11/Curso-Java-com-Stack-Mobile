b52e95d8fc364bda71ad8fa0b5fab5560d9835f2
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-09-10 00:08:57 -0300

    Update and rename OperadoresAritmeticos.java to Operadores.java

--- Operadores.java
+++ Operadores.java
@@ -15,10 +15,12 @@ public class Operadores {
         */
 
         int n1;
+        System.out.println("Insira o número 1:");
         Scanner first = new Scanner(System.in);
         n1 = first.nextInt();
 
         int n2;
+        System.out.println("Insira o segundo:");
         Scanner second = new Scanner(System.in);
         n2 = second.nextInt();
 
@@ -27,6 +29,16 @@ public class Operadores {
         System.out.println(n1 / n2);
         System.out.println(n1 % n2);
 
+          /*
 
+        Operadores Relacionais (>, >=, <, <=)
+        Operadores de igualdade (==, !=)
+
+         */
+
+        System.out.println(n1 == n2); //true or false
+        System.out.println(n1 != n2);
+        System.out.println(n1 >= n2);
+        System.out.println(n1 <= n2);
     }
 }
