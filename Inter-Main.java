1a5980dae08c8976bc904615b29f7ed9128a6de1
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-09-11 02:26:33 -0300

    Create Inter-Main.java

---
+++ Inter-Main.java
@@ -0,0 +1,14 @@
+package POO;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+    Carro carro = new Carro();
+    carro.acelerar("Carro está em movimento");
+    carro.desacelerar("Carro está desacelerando");
+
+    Moto moto = new Moto();
+    moto.acelerar("");
+    moto.desacelerar("");
+    }
+}
