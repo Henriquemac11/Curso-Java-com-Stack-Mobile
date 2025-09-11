e0ed2a5f41e87b070296595fa5d8a37b1d448a43
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-09-11 01:46:12 -0300

    Create H-Moto.java

---
+++ H-Moto.java
@@ -0,0 +1,10 @@
+package POO;
+
+import POO.Heranca.Veiculo;
+
+public class Moto extends Veiculo {
+
+    public Moto(String cor, String modelo, String placa) {
+        super(cor, modelo, placa);
+    }
+}
