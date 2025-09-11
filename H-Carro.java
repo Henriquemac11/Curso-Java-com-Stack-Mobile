d29eb52e3a7130c67005f0a9121ef7133bf49b4a
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-09-11 01:45:45 -0300

    Create H-Carro.java

---
+++ H-Carro.java
@@ -0,0 +1,11 @@
+package POO;
+
+import POO.Heranca.Veiculo;
+
+public class Carro extends Veiculo {
+
+
+    public Carro(String cor, String modelo, String placa) {
+        super(cor, modelo, placa);
+    }
+}
